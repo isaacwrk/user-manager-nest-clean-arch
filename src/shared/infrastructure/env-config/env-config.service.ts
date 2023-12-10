2e9f67dde4bb4f183c4aa1971b052767f90e170b
isaac.santos <isaac.santos@vsoft.com.br>
2023-12-10 15:41:48 -0300

    feat: create users initial arch

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -1,15 +1,15 @@
-import { Injectable } from '@nestjs/common';
-import { EnvConfig } from './env-config.interface';
-import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common'
+import { EnvConfig } from './env-config.interface'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
 export class EnvConfigService implements EnvConfig {
   constructor(private configService: ConfigService) {}
 
   getAppPort(): number {
-    return Number(this.configService.get<number>('PORT'));
+    return Number(this.configService.get<number>('PORT'))
   }
   getNodeEnv(): string {
-    return this.configService.get<string>('NODE_ENV');
+    return this.configService.get<string>('NODE_ENV')
   }
 }
