afc6f4b112610a7b70d5eea945deb4c629fce54c
isaac.santos <isaac.santos@vsoft.com.br>
2023-12-10 17:54:40 -0300

    refactor: create user data builder for user entity test

--- src/users/domain/entities/__tests__/unity/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unity/user.entity.spec.ts
@@ -1,15 +1,11 @@
-import { faker } from '@faker-js/faker'
 import { UserEntity, UserProps } from '../../user.entity'
+import { userDataBuilder } from '../../../testing/helpers/user-data-builder'
 
 describe('UserEntity unit tests', () => {
   let props: UserProps
   let sut: UserEntity
   beforeEach(() => {
-    props = {
-      name: faker.person.fullName(),
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    }
+    props = userDataBuilder({})
     sut = new UserEntity(props)
   })
 
